e9f5d359f396214227d541d818778eaf3d4ca9de
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-03-20 03:15:33 +0530

    again fixing case sensitivity

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from 'react-router-dom'
-import { AppProvider } from './context/contextapi.jsx'
+import { AppProvider } from './context/contextapi'
 
 createRoot(document.getElementById('root')).render( 
   <StrictMode>
