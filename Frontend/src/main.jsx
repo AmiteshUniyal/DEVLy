8c1846230689f6cd1238fdb3b9d38287f48b3b14
Amitesh Uniyal <amiteshuniyalz7@gmail.com>
2025-03-21 00:25:36 +0530

    changes in routing style

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,15 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 import { AppProvider } from "./context/contextapi";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <AppProvider> 
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App /> 
     </AppProvider>
   </StrictMode>
 );
